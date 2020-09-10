22f59e6dbdacf18d5610d549b705c8371b84fb7b
Vanessa Garcia <vanessag0501@gmail.com>
2020-09-09 20:20:21 -0400

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Vanessa Garcia on 9/9/20.
+//  Copyright © 2020 Vanessa Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
